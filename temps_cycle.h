c2644821f58b46cb823875139f4a92e0320e42a7
gibertjules <133090934+gibertjules@users.noreply.github.com>
2023-12-03 19:56:03 +0100

    Create temps_cycle.h

---
+++ temps_cycle.h
@@ -0,0 +1,8 @@
+//
+// Created by jules on 02/12/23.
+//
+
+#ifndef TEMPS_DE_CYCLE_TEMPS_CYCLE_H
+#define TEMPS_DE_CYCLE_TEMPS_CYCLE_H
+double readTempsCycle();
+#endif //TEMPS_DE_CYCLE_TEMPS_CYCLE_H
